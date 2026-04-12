601a1843e4334653c672313b001a196d19b7216f
richardgms <richardgms001@gmail.com>
2026-04-12 01:51:59 -0300

    chore: bump para v1.30.1

--- src/components/admin-panel/app-sidebar.tsx
+++ src/components/admin-panel/app-sidebar.tsx
@@ -113,7 +113,7 @@ export function AppSidebar() {
                                 </div>
                                 <div className="flex flex-col gap-0.5 leading-none">
                                     <span className="font-semibold">RMenu Digital</span>
-                                    <span className="">v1.31.0</span>
+                                    <span className="">v1.30.1</span>
                                 </div>
                             </div>
                         </SidebarMenuButton>
